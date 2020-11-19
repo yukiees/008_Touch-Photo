e2d0cc62513656b6a07f66b20ca038cb969233ab
yukiees <yukiii8989@gmail.com>
2020-11-19 09:46:52 +0900

    UI

--- 008_Touch+Photo/ViewController.swift
+++ 008_Touch+Photo/ViewController.swift
@@ -8,11 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var imageNameArray: [String] = ["hana","hoshi","onpu","shitsumon"]
+    
+    var imageIndex: Int = 0
+    
+    @IBOutlet weak var haikeiImageView: UIImageView!
+    
+    var imageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func selectedFirst(_ sender: Any) {
+    }
+    
+    @IBAction func selectedSecond(_ sender: Any) {
+    }
+    
+    @IBAction func selectedThird(_ sender: Any) {
+    }
+    
+    @IBAction func selectedFourth(_ sender: Any) {
+    }
+    
 
 
 }
